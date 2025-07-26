5699fcb4d503b98f442edd6220be420a310c788f
ROSHINI S <roshinis24bit048@skasc.ac.in>
2025-07-26 19:44:26 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,30 @@
+print("develper")
+p="python" 
+print(p)
+
+#multiline string:
+z="""you can only win when your mind is stronger than ypur feelings"""
+print(z)
+
+#strings are array:
+y="Data analytics!!"
+print(y[3])
+
+#looping through strings:
+for b in "hadoop":
+    print(b)
+
+#length of a string:
+u="figma"
+print(len(u))
+
+#check string:
+h="Everything has its own time"
+print("own" in h)
+
+#checks if NOT:
+d="faith can move mountains"
+print("can" not in d)
+
+
+
